3a459c66cbf82bf356c0b5b4a5ddbcf1a4b5702a
juancalle19 <jesteban.2741gmail.com>
2021-09-21 21:56:38 -0500

    add vehicletype

---
+++ VehiclesAPI/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiclesAPI.Data.Entities;
+
+namespace VehiclesAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique(); ;
+        }
+    }
+}
